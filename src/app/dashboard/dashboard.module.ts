bea3aaa794f98d3baa7fca71fbbc8a9b7df851fd
Alex Botelho <alexbotelho@ALEX01.hallyzsystem.com>
2021-07-15 13:03:16 -0300

    Finalized

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DadosService } from './dados.service';
+import { DashboardComponent } from './dashboard.component';
+
+@NgModule({
+  declarations: [
+    DashboardComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    DashboardComponent
+  ],
+  providers: [
+    DadosService
+  ]
+})
+export class DashboardModule { }
